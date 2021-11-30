f0656a94328b2b0f66ea2499d72466b82888fe04
Justin W Soto <soto@pdx.edu>
2021-11-29 17:49:43 -0800

    added components to appJS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Character } from './components/Character/Character';
+import { Display } from './components/Display/Display';
+import { Picker } from './components/Picker/Picker';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        <p>
-          <h1>h3llo?</h1>
-        </p>
-        
+        <h1>h3llo?</h1>
       </header>
+      
+      <Character />
+      <Display />
+      <Picker />
+
+        
     </div>
   );
 }
